f9c97a101d54b121b86360961d16265a244a097b
dombonifacio <nikkibonifacio184@gmail.com>
2023-06-23 22:57:28 -0700

    Added React Pagination

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,21 +1,31 @@
 import axios from "axios"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
+
+
+// third party librarie
+import ReactPaginate from "react-paginate"
 
 export const HomePage = () => {
 
+    // [...movies], data.results
+    const [movies, setMovies] = useState([])
     
-
+    // declare var for total pages (tmdb gives 1000 total pages only)
+    
     useEffect(() => {
         getMoviesData()
+        
     }, [])
 
     const getMoviesData = () => {
-        axios.get('https://api.themoviedb.org/3/discover/movie?page=3&api_key=e1dca25da22a67a9069503b710cfa699')
+        axios.get('https://api.themoviedb.org/3/discover/movie?api_key=e1dca25da22a67a9069503b710cfa699&page=2')
         .then(({data}) => {
-            console.log(data)
+            
+            setMovies([...movies, data.results])
+
         })
     }
-
+    console.log(movies)
     return (
         <div>
            <h1>
